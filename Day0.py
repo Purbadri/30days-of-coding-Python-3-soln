93c581dfaeae95dc4aad8cc4f0734e45c0894eed
Purbadri <57594633+Purbadri@users.noreply.github.com>
2021-07-22 20:33:58 +0530

    Add files via upload

---
+++ Day0.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#DAY 0 (30 days of coding)
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+print(input_string)
+
+
+
+
